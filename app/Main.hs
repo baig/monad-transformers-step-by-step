a78bbfe1b1d208d9aa72c269c88fa87b5f7deeaa
Wasif Hasan Baig <wasif@Wasifs-MacBook-Pro.local>
2017-12-07 09:58:55 -0500

    Section 2.4

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,12 @@ import Transform
 import qualified Data.Map as Map
 
 -- 12 + ((λx → x)(4 + 2))
-exampleExp1 = (Abs "x" (Var "x")) `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+exampleExp1 = (Lit 12) `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
 exampleExp2 = (Plus (Lit 1) (Abs "x" (Var "x")))
 exampleExp3 = (App (Lit 4) (Lit 4))
 
 main :: IO ()
 main = do
-    putStrLn $ show $ runEval Map.empty (eval exampleExp1)
-    putStrLn $ show $ runEval Map.empty (eval exampleExp2)
-    putStrLn $ show $ runEval Map.empty (eval exampleExp3)
+    putStrLn $ show $ runEval Map.empty 0 (eval exampleExp1)
+    putStrLn $ show $ runEval Map.empty 0 (eval exampleExp2)
+    putStrLn $ show $ runEval Map.empty 0 (eval exampleExp3)
--- src/Transform.hs
+++ src/Transform.hs
@@ -5,6 +5,7 @@ import qualified Data.Map as Map
 import Control.Monad.Identity
 import Control.Monad.Error
 import Control.Monad.Reader
+import Control.Monad.State
 
 type Name = String
 
@@ -25,26 +26,36 @@ data Value = IntVal Integer
 
 -- In order to use monad transformers, it is necessary to express functions in monadic style. That means that the programmer needs to impose sequencing on all monadic operations using do notation, and to use the return function in order to specify the result of a function.
 -- We define a monad in which the evaluator will be defined. The following type synonym defines Eval α as a synonym for the type Identity α. Identity is a monad imported from Control.Monad.Identity, which is perhaps the simplest monad imaginable: it defines the standard return and >= operations for constructing operations in the monad, and additionally a function runIdentity to execute such operations. Other than that, the identity monad has no effect.
-type Eval a = ReaderT Env (ErrorT String Identity) a
+type Eval a = ReaderT Env (ErrorT String (StateT Integer Identity)) a
 
 -- For readability, we also define a function runEval1, which simply calls runIdentity.
-runEval :: Env -> Eval a -> Either String a
-runEval env ev = runIdentity $ runErrorT $ runReaderT ev env
+runEval :: Env -> Integer -> Eval a -> (Either String a, Integer)
+runEval env st ev = runIdentity $ runStateT (runErrorT $ runReaderT ev env) st
+
+tick :: (Num s, MonadState s m) => m ()
+tick = do
+      st <- get
+      put (st + 1)
 
 eval :: Exp -> Eval Value
-eval (Lit i) = return $ IntVal i
-eval (Var n) = do env <- ask
+eval (Lit i) = do tick
+                  return $ IntVal i
+eval (Var n) = do tick
+                  env <- ask
                   case Map.lookup n env of
                         Nothing -> throwError $ "unbound variable" ++ n
                         Just v -> return v
-eval (Plus e1 e2) = do  e1' <- eval e1
+eval (Plus e1 e2) = do  tick
+                        e1' <- eval e1
                         e2' <- eval e2
                         case (e1', e2') of
                               (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2) -- Whenever one of the addition operands evaluates to a non-number, the pattern matching in the let expression will fail, also terminating the program with an error message.
                               _ -> throwError "type error"
-eval (Abs n e) = do env <- ask
+eval (Abs n e) = do tick
+                    env <- ask
                     return $ FunVal env n e
-eval (App e1 e2) = do   funVal <- eval e1
+eval (App e1 e2) = do   tick
+                        funVal <- eval e1
                         intVal <- eval e2
                         case funVal of
                               FunVal env' n body -> local (const $ Map.insert n intVal env') (eval body) -- Function application proceeds similar to addition, by first evaluating the function and the argument. The first ex- pression must evaluate to a functional value, whose body is then evaluated in the captured environment, extended with the binding of the function parameter to the argument value. The case expression used here to deconstruct the functional value introduces another error possibility.
